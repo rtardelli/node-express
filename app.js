fa3766dc49ec29a501aa6e0d23c589f3cbdaa371
= <=>
2020-05-20 16:15:16 -0300

    Adding openapi endpoint to documentation

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+
+const YAML = require('yamljs');
+const apiDocument = YAML.load('./service-openapi.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocument));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
