61caf611e4da90a70834b9cf7b480c25e668ea77
= <=>
2020-05-27 14:19:24 -0300

    Adding morgan dependecy

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const swaggerUi = require('swagger-ui-express');
+const morgan = require("morgan");
+const swaggerUi = require("swagger-ui-express");
 
-const YAML = require('yamljs');
-const apiDocument = YAML.load('./service-openapi.yaml');
+const YAML = require("yamljs");
+const apiDocument = YAML.load("./service-openapi.yaml");
 
-const userRouter = require('./routes/user');
+const userRouter = require("./routes/user");
 
 const port = process.env.PORT || 3000;
 
-// Routes
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocument));
-app.use("users", userRouter);
+// Middleware
+app.use(morgan("combined"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Routes
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiDocument));
+app.use("/users", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log('That is working on port ' + port);
+  console.log("That is working on port " + port);
 });
