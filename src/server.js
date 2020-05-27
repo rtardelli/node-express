3f26f5c958735dc3e36de5ef6a4c393323d20de2
= <=>
2020-05-27 15:43:13 -0300

    Reorganizing server main file

---
+++ src/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const morgan = require("morgan");
+const fs = require('fs');
+const path = require('path')
+const swaggerUi = require("swagger-ui-express");
+
+const YAML = require("yamljs");
+const apiDocument = YAML.load("src/service-openapi.yaml");
+
+const userRouter = require("./routes/user");
+
+class App {
+    constructor() {
+        this.express = express();
+
+        this.database();
+        this.middlewares();
+        this.routes();
+
+        const port = process.env.PORT || 3000;
+        this.express.listen(port, () => {
+            console.log("That is working on port " + port);
+        });
+    }
+
+    database() {
+
+    }
+
+    middlewares() {
+        const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" })
+        this.express.use(morgan("combined", { stream: accessLogStream }))
+    }
+
+    routes() {
+        this.express.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiDocument));
+        this.express.use("/users", userRouter);
+
+        this.express.get("/", (req, res) => {
+            res.send("Hello World!");
+        });
+    }
+}
+
+module.exports = new App().express;
